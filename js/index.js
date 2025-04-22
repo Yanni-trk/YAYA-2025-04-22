09c0cea62b279eb20dc92dd30ab82cc836e5e427
Yanni-trk <yanni.terki@gmail.com>
2025-04-22 23:57:50 +0200

    blueloaded

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@
 function loadJs() {
     var jsLoaded = document.querySelector('#js-loaded');
     jsLoaded.innerHTML = "JS OK";
+    jsLoaded.style.backgroundColor = "cornflowerblue";
 }
 loadJs();
-
