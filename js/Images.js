8a4c04c72323dbee3b83c9593c3e4fa355cb7433
Yaya <yanni.terki@gmail.com>
2025-04-24 08:49:06 +0100

    getter setter

--- js/Images.js
+++ js/Images.js
@@ -1,41 +1,48 @@
 const REST_ADR = 'http://localhost:5679'
+/**
+ * class pour la manipulation du REST
+ */
 class Images extends Array {
+    //ici je met l'url des ressources sur le srveur REST pour y acceder
     #ressourcePath;
     /**
-     * constructeur d'image
+     * @type Promise<Images>
+     */
+    #loadPromise = undefined;
+
+    get promiseImages(){
+        return this.#loadPromise;
+    }
+    set promiseImages(value){
+        this.#loadPromise=value;
+    }
+    /**
+     * constructeur d'images
      * @param {string} ressourcePath chemin dans le REST des images
      */
     constructor(ressourcePath = '/images') {
         super();
         this.#ressourcePath = ressourcePath;
-    }
 
+    }/**
+     * chargement rest des datas d'images
+     */
     loadRessources() {
-        fetch(REST_ADR + this.#ressourcePath)
+        if(undefined !== this.#loadPromise)
+      this.#loadPromise = fetch(REST_ADR + this.#ressourcePath)
             .then(r => r.json())
             .then((arr) => {
                 this.splice(0);
                 this.push(...arr);
-                console.table(this)
+                console.table(this);
+                return this;
             })
-
+            return this.#loadPromise;
     }
 
 }
-
 /**
- * remplacer d'image
- * @param {ojbect} origineImage image a remplace
- * @param {ojbect} newImage image de substitution 
- * @return {object} image remplacé
- */
-
-
-/**
- * instance principale de toutes les images de l'app
+ * instance principales de toutes les images de l'app
  */
 const images = new Images();
-
-images.loadRessources();
-
-
+images.loadRessources(()=>{});
